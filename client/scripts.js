9079303f7414b9b9380c98ab37e9dd8a435a31e1
Matthew DeJonge <mdejonge@terryberry.com>
2026-03-16 19:21:59 -0400

    main: editorconfig and search implementation

--- client/scripts.js
+++ client/scripts.js
@@ -1,5 +1,11 @@
 const API_BASE = `${window.location.protocol}//${window.location.hostname}:8081`;
-fetch(`${API_BASE}/api/recipe`).then(result => result.json()).then(data => {
+
+function search() {
+  const currentUrl = new URL(location.href);
+  const apiUrl = new URL(`${API_BASE}/api/recipe`);
+  apiUrl.searchParams = currentUrl.searchParams;
+  fetch(apiUrl).then(result => result.json()).then(data => {
     const recipes = document.getElementById('recipes');
     recipes.innerText = JSON.stringify(data);
-});
+  });
+}

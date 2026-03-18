38868e3445024f029c40ca4d4fad764653492555
Matthew DeJonge <mdejonge@terryberry.com>
2026-03-18 17:15:15 -0400

    main: fixing results page

--- client/scripts.js
+++ client/scripts.js
@@ -1,6 +1,13 @@
 const CLIENT_BASE = `${window.location.protocol}//${window.location.hostname}:8080`;
 const API_BASE = `${window.location.protocol}//${window.location.hostname}:8081`;
 
+window.onload = () => {
+  const page = document.getElementById('page');
+  if (page.content === 'results') {
+    getResults();
+  }
+};
+
 const tags = new Set();
 
 function tag(value) {
@@ -14,7 +21,7 @@ function tag(value) {
 
 function updateSearchUrl() {
   const searchBar = document.getElementById('search-bar');
-  const resultsUrl = new URL(`${CLIENT_BASE}/results`);
+  const resultsUrl = new URL(`${CLIENT_BASE}/results.html`);
   resultsUrl.searchParams.append('search', searchBar.value);
   tags.forEach(tag => resultsUrl.searchParams.append('tag', tag));
   document.getElementById('search-results').href = resultsUrl.href;
